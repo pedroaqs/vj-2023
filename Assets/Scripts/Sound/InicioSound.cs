db6747472fec3be08a0ef2315b5a171129c1c6d9
juniorch1014 <chuquilingue@gmail.com>
2023-06-22 20:39:01 -0500

    Agregando Player

--- Assets/Scripts/Sound/InicioSound.cs
+++ Assets/Scripts/Sound/InicioSound.cs
@@ -1,31 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InicioSound : MonoBehaviour
 {   
 
     public AudioClip musicaClip;
     private AudioSource musicaAudioSource;
-    private bool band;
+
     int aux;
+    int nivelVol;
+
+    private static InicioSound instance;
+
+ 
+    public TMP_Text vol;
     // Start is called before the first frame update
     
-    void Awake() 
-    {
-        DontDestroyOnLoad(gameObject);  
-    }
+    // void Awake() 
+    // {
+    //     DontDestroyOnLoad(gameObject);  
+    // }
     
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     
     void Start()
     {
-         //Sound
-        musicaAudioSource = gameObject.AddComponent<AudioSource>();
-        musicaAudioSource.clip = musicaClip;
-     
-        musicaAudioSource.loop = true;
-        musicaAudioSource.Play();
-        aux = 0;
+        
+        PlaySound();
+        nivelVol = 100;
+
     }
 
     // Update is called once per frame
@@ -36,10 +53,52 @@ public class InicioSound : MonoBehaviour
     public void muted (){
         if(aux == 0){
             musicaAudioSource.mute = true;
+            vol.text = "0";
             aux++;
         }else{
             musicaAudioSource.mute = false;
+            vol.text = nivelVol.ToString();
             aux = 0;
         }
     }
+
+    public void PlaySound()
+    {
+         //Sound
+        musicaAudioSource = gameObject.AddComponent<AudioSource>();
+        musicaAudioSource.clip = musicaClip;
+        musicaAudioSource.loop = true;
+        musicaAudioSource.Play();
+        aux = 0;
+    }
+
+    public void StopSound()
+    {
+         musicaAudioSource.Stop();
+    }
+
+    public void AumentarVolumen () {
+        if(musicaAudioSource.volume < 1f) {
+            nivelVol += 10;
+            musicaAudioSource.volume += 0.1f;
+            vol.text = nivelVol.ToString();
+    
+            
+        }
+    }
+    public void DisminuirVolumen () {
+        if(musicaAudioSource.volume > 0f) {
+            nivelVol -= 10;
+            musicaAudioSource.volume -= 0.1f;
+            vol.text = nivelVol.ToString();
+           
+        }
+    }
+
+    public void cambiarText () {
+        vol.text = "VOLUMEN";
+        
+    }
+
+
 }
---
+++ Assets/Scripts/Sound/OpcionesSound.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpcionesSound : MonoBehaviour
+{
+    // private InicioSound iSound;
+    // int aux;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //    // iSound = new InicioSound();
+    //     aux = 0;
+    
+    // }
+    
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    // public void muted (){
+    //     if(aux == 0){
+    //         iSound.musicaAudioSource.mute = true;
+    //         aux++;
+    //     }else{
+    //         iSound.musicaAudioSource.mute = false;
+    //         aux = 0;
+    //     }
+    // }
+}
